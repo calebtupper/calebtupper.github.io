4d56c38d8aec13b6d749ce100486e94ee677b1df
Caleb Tupper <calebtupper@gmail.com>
2021-03-17 10:00:23 -0400

    Final Site Redesign

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,6 @@
 $(document).ready(function(){
     $("div.light-dark").click(function(){
             $("body").toggleClass("dark-mode"); 
-            if ($("body").hasClass("dark-mode")) {
-                $("a.nav-logo img").attr("src","assets/img/tm-logo-web-white.png");
-            }
-            else{
-                $("a.nav-logo img").attr("src","assets/img/tm-logo-web-black.png");
-            }
         });
     
     $("nav ul li").click(function(){
