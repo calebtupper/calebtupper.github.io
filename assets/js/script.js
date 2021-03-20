12aae3b66b9649e199470dd1d2e042776f0794aa
Caleb Tupper <calebtupper@gmail.com>
2021-03-20 13:51:38 -0400

    Add Light/Dark Cookie

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,23 @@
-/*JQuery*/
+/*JQuery Light-Dark Mode Selector and Cookie*/
 $(document).ready(function(){
+    if($("body").hasClass("dark-mode")){
+               $("body").toggleClass("dark-mode");
+            }
+    else{}
     $("div.light-dark").click(function(){
             $("body").toggleClass("dark-mode");
-            setCookie("color","dark",1);
+            if($("body").hasClass("dark-mode")){
+                setCookie("color","dark",30);
+            }
+            else(){}   
+                setCookie("color","light",30);
         });
 }); 
 
-function setCookie(cname, cvalue, exhours){
+/*Set JS Cookie*/
+function setCookie(cname, cvalue, exdays){
     var date = new Date();
-    date.setTime(date.getTime()+exhours*60*60*1000);
+    date.setTime(date.getTime()+exdays*24*60*60*1000);
     var expires = "; expires =" + date.toGMTString();
     document.cookie = cname+"="+cvalue+expires+";path=/"
 }
